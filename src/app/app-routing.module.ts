7099fd65b30d04bc9ab6638d3691be3129932d44
Bret Morris <morris.bret@gmail.com>
2017-12-14 11:26:52 -0800

    Add ua5 sugar.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BaseComponent } from './components/base/base.component';
+import { PageComponent } from './components/page/page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: BaseComponent,
+    children: [
+      {
+        path: '**',
+        component: PageComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { BaseComponent } from './components/base/base.component';
+import { PageComponent } from './components/page/page.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BaseComponent,
+    PageComponent
   ],
   imports: [
     BrowserModule,
