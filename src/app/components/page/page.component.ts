404151dff45c25ed8eac9da80b882ff9638bd6f6
Bret Morris <morris.bret@gmail.com>
2018-01-02 14:03:57 -0800

    Manually tell prerender.io when page is ready to be prerendered

--- src/app/components/page/page.component.ts
+++ src/app/components/page/page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var window;
 
 @Component({
   selector: 'app-page',
@@ -10,6 +11,9 @@ export class PageComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    // Manually tell prerender.io that the page is ready to be prerendered.
+    // Typically this should happen after one manually updates the meta tags.
+    window.prerenderReady = true;
   }
 
 }
