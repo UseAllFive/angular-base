1ecf2dbd653af7b6c2c8430174167e76adbf3bbd
Bret Morris <morris.bret@gmail.com>
2018-01-29 11:35:07 -0800

    Add contentful

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { MarkdownToHtmlModule } from 'markdown-to-html-pipe';
+import { ContentfulModule } from 'angular-contentful-service';
 import { AppComponent } from './app.component';
 import { BaseComponent } from './components/base/base.component';
 import { PageComponent } from './components/page/page.component';
-
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { PageComponent } from './components/page/page.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MarkdownToHtmlModule
+    MarkdownToHtmlModule,
+    ContentfulModule.forRoot(environment.contentful)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,10 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  contentful: {
+    space: '',
+    accessToken: '',
+    host: 'preview.contentful.com'
+  }
 };
